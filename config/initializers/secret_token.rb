e4237cf0a28da9901c69c2d8ed24b23ac106c831
eduardoschneiders <eduardo.m.schneiders@gmail.com>
2012-08-30 14:52:23 -0300

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TemplateProject::Application.config.secret_token = '581233087a75cf86de5ddecda1e273aae688fe1ab9891776ec1f0955e055fc8387ff27f0e912d702d0853fe9bbcdc33f3165f6d6f3b4e26002e6229ee1cf3cdb'
